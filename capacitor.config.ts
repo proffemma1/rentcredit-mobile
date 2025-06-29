0aa01eeeb3d0c9ae52b41e57f3e1b38ba914e7a2
proffemma1 <ogu@pegasusestates.net>
2025-06-29 14:36:53 -0400

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,19 +3,9 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.rentcredit.app',
   appName: 'RentCredit',
-  webDir: 'mobile-dist',
+  webDir: 'www',
   server: {
     androidScheme: 'https'
-  },
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 2000,
-      backgroundColor: "#8B5CF6"
-    },
-    StatusBar: {
-      style: "light",
-      backgroundColor: "#8B5CF6"
-    }
   }
 };
 
